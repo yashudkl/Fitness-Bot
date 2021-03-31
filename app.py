c9b31668c639128c5705469018e83fc7bacd025a
Eclipsu <stharj219@gmail.com>
2021-03-31 16:49:47 +0545

    routine command added

--- app.py
+++ app.py
@@ -16,18 +16,94 @@ def get_token():
     return bot_token
 
 
-client = commands.Bot(command_prefix = "test ")   # global bot delcaration.
+client = commands.Bot(command_prefix = "<3 ")   # global bot delcaration.
+
+def get_routine():
+    """
+    get details about routines from routine.json
+
+    Args:
+        openfile, json_object, pairs = variables req by json module (honestly idk what it does). 
+        cr_year, cr_month, cr_day    = Current year/month/day.
+        week_days                    = array of weeks (starts with monday and ends with monday to fit internation week format).
+        week_num                     = a int which acts as index to get current week from week_days array.
+        today_routine                = return value containing informations about the workouts.
+    Returns:
+        wokrout, video, thumnail_link, gif_link, rounds, etc.
+    """
+
+    with open('routine.json', 'r') as openfile:
+        json_object = json.load(openfile)
+        pairs = json_object.items()
+
+        week_days = ['monday', 'tuesday', 'wednesday', 'thirsday', 'friday', 'saturday', 'sunday']
+        cr_year = datetime.today().strftime('%Y')
+        cr_month = datetime.today().strftime('%m')
+        cr_day = datetime.today().strftime('%d')
+        week_num  = dt.date(int(cr_year), int(cr_month), int(cr_day)).weekday() 
+
+        routine_title = json_object[str(week_days[week_num])]['title']
+        _1_title = json_object[str(week_days[week_num])]['1']['name']
+        _1_reps  = json_object[str(week_days[week_num])]['1']['reps']
+        _1_link  = json_object[str(week_days[week_num])]['1']['link']
+
+        _2_title = json_object[str(week_days[week_num])]['2']['name']
+        _2_reps  = json_object[str(week_days[week_num])]['2']['reps']
+        _2_link  = json_object[str(week_days[week_num])]['2']['link']
+
+        _3_title = json_object[str(week_days[week_num])]['3']['name']
+        _3_reps  = json_object[str(week_days[week_num])]['3']['reps']
+        _3_link  = json_object[str(week_days[week_num])]['3']['link']
+
+        _4_title = json_object[str(week_days[week_num])]['4']['name']
+        _4_reps  = json_object[str(week_days[week_num])]['4']['reps']
+        _4_link  = json_object[str(week_days[week_num])]['4']['link']
+
+        _5_title = json_object[str(week_days[week_num])]['5']['name']
+        _5_reps  = json_object[str(week_days[week_num])]['5']['reps']
+        _5_link  = json_object[str(week_days[week_num])]['5']['link']
+
+        _6_title = json_object[str(week_days[week_num])]['6']['name']
+        _6_reps  = json_object[str(week_days[week_num])]['6']['reps']
+        _6_link  = json_object[str(week_days[week_num])]['6']['link']
+
+        _7_title = json_object[str(week_days[week_num])]['7']['name']
+        _7_reps  = json_object[str(week_days[week_num])]['7']['reps']
+        _7_link  = json_object[str(week_days[week_num])]['7']['link']
+    return routine_title, _1_title, _1_reps, _1_link, _2_title, _2_reps, _2_link, _3_title, _3_reps, _3_link, _4_title, _4_reps, _4_link, _5_title, _5_reps, _5_link, _6_title, _6_reps, _6_link, _7_title, _7_reps, _7_link
+
+
+
+
 # bot on ready function
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game('<3/ Workout'))
     print(f'{client.user.name} deployed!')
 
+
+@client.command()
+async def schedule(ctx):
+    title,_1_title, _1_reps, _1_link,  _2_title, _2_reps, _2_link, _3_title, _3_reps, _3_link, _4_title, _4_reps, _4_link, _5_title, _5_reps, _5_link, _6_title, _6_reps, _6_link, _7_title, _7_reps, _7_link = get_routine()
+    schedule_embed = discord.Embed(
+        title = "📋",
+        color = discord.Color.purple(),
+        description = 'Do all reps for 4 rounds each!'
+    )
+    schedule_embed.add_field(name = _1_title,  value  = f"{_1_reps} reps", inline = True)
+    schedule_embed.add_field(name = _2_title,  value  = f"{_2_reps} reps", inline = True)
+    schedule_embed.add_field(name = _3_title,  value  = f"{_3_reps} reps", inline = True)
+    schedule_embed.add_field(name = _4_title,  value  = f"{_4_reps} reps", inline = True)
+    schedule_embed.add_field(name = _5_title,  value  = f"{_5_reps} reps", inline = True)
+    schedule_embed.add_field(name = _6_title,  value  = f"{_6_reps} reps", inline = True)
+    
+    await ctx.send(embed = schedule_embed)
+
 # lists outs schedule of the week
 @client.command()
 async def routine(ctx):
     routine_embed = discord.Embed(
-        title = "📋: Routine",
+        title = "\📋: Routine",
         color = discord.Color.purple(),
         description = "7 days, 6 grind, 1 rest."
     )
@@ -42,99 +118,101 @@ async def routine(ctx):
     await ctx.send(embed = routine_embed)
 
 
-@client.command() 
-async def schedule(ctx):
-    embed = discord.Embed(
-        title= "Schedule ",
-        color = discord.Color.purple(),
-        description = "Follow the Schedule provided."
-        )
-    embed.add_field(name="Weight gain", value="Workouts:-Push , Pull , Leg", inline=True)
-    embed.add_field(name="Fat Loss", value="Workouts:- Cardio ", inline=True)
-    embed.set_image(url='https://th.bing.com/th/id/OIP.1NfnV_pv0VI4QaQJBgHdVQHaEc?w=300&h=180&c=7&o=5&pid=1.7')
-    embed.set_thumbnail(url='https://th.bing.com/th/id/OIP.W1Sy8iy9IUMxY5nxgUVeZwHaHa?w=182&h=182&c=7&o=5&pid=1.7')
-    await ctx.send(embed=embed)
+
+
+# @client.command() 
+# async def schedule(ctx):
+#     embed = discord.Embed(
+#         title= "Schedule ",
+#         color = discord.Color.purple(),
+#         description = "Follow the Schedule provided."
+#         )
+#     embed.add_field(name="Weight gain", value="Workouts:-Push , Pull , Leg", inline=True)
+#     embed.add_field(name="Fat Loss", value="Workouts:- Cardio ", inline=True)
+#     embed.set_image(url='https://th.bing.com/th/id/OIP.1NfnV_pv0VI4QaQJBgHdVQHaEc?w=300&h=180&c=7&o=5&pid=1.7')
+#     embed.set_thumbnail(url='https://th.bing.com/th/id/OIP.W1Sy8iy9IUMxY5nxgUVeZwHaHa?w=182&h=182&c=7&o=5&pid=1.7')
+#     await ctx.send(embed=embed)
 
 # information about pull
-@client.command() 
-async def push(ctx):
-    embed = discord.Embed(
-        title= "Push Day Workouts",
-        color = discord.Color.purple(),
-        description = "We have some push day workouts here:-"
-    )
-    embed.add_field(name="Weighted Pushups", value="Do Pushups (<3 weightedpushups)", inline=True)
-    embed.add_field(name="Dips", value="Do some dips (<3 dips)", inline=True)
-    await ctx.send(embed=embed)
+# @client.command() 
+# async def push(ctx):
+#     embed = discord.Embed(
+#         title= "Push Day Workouts",
+#         color = discord.Color.purple(),
+#         description = "We have some push day workouts here:-"
+#     )
+#     embed.add_field(name="Weighted Pushups", value="Do Pushups (<3 weightedpushups)", inline=True)
+#     embed.add_field(name="Dips", value="Do some dips (<3 dips)", inline=True)
+#     await ctx.send(embed=embed)
 
-# information about pushups
-@client.command() 
-async def weightedpushups(ctx):
-    embed = discord.Embed(
-        title= "How to do Weighted pushups",
-        color = discord.Color.purple(),
-        description = "Just add weights (book in bags) in Normal pushups"
-    )
-    embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825259653846925342/SmartVapidCaribou-small.gif')
-    await ctx.send(embed=embed)
+# # information about pushups
+# @client.command() 
+# async def weightedpushups(ctx):
+#     embed = discord.Embed(
+#         title= "How to do Weighted pushups",
+#         color = discord.Color.purple(),
+#         description = "Just add weights (book in bags) in Normal pushups"
+#     )
+#     embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825259653846925342/SmartVapidCaribou-small.gif')
+#     await ctx.send(embed=embed)
 
-# information about dips
-@client.command() 
-async def dips(ctx):
-    embed = discord.Embed(
-        title= "How to do Dips",
-        color = discord.Color.purple(),
-        description = "See in GIF below"
-    )
-    embed.set_image(url='')
-    await ctx.send(embed=embed)
+# # information about dips
+# @client.command() 
+# async def dips(ctx):
+#     embed = discord.Embed(
+#         title= "How to do Dips",
+#         color = discord.Color.purple(),
+#         description = "See in GIF below"
+#     )
+#     embed.set_image(url='')
+#     await ctx.send(embed=embed)
 
-# information about pull
-@client.command() 
-async def pull(ctx):
-    embed = discord.Embed(
-        title= "Pull Day Workouts",
-        color = discord.Color.purple(),
-        description = "We have some pull day workouts here:-"
-    )
-    embed.add_field(name="Pullups", value="Do (<3 pullups)", inline=True)
-    embed.add_field(name="Bicep curls", value="Do (<3 bicepcurl)", inline=True)
+# # information about pull
+# @client.command() 
+# async def pull(ctx):
+#     embed = discord.Embed(
+#         title= "Pull Day Workouts",
+#         color = discord.Color.purple(),
+#         description = "We have some pull day workouts here:-"
+#     )
+#     embed.add_field(name="Pullups", value="Do (<3 pullups)", inline=True)
+#     embed.add_field(name="Bicep curls", value="Do (<3 bicepcurl)", inline=True)
 
-    await ctx.send(embed=embed)
+#     await ctx.send(embed=embed)
 
-# information about pullups
-@client.command() 
-async def pullups(ctx):
-    embed = discord.Embed(
-        title= "How to do Pullups",
-        color = discord.Color.purple(),
-        description = "See in GIF below"
-    )
-    embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825262153883451422/4Fhl.gif')
+# # information about pullups
+# @client.command() 
+# async def pullups(ctx):
+#     embed = discord.Embed(
+#         title= "How to do Pullups",
+#         color = discord.Color.purple(),
+#         description = "See in GIF below"
+#     )
+#     embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825262153883451422/4Fhl.gif')
     
-    await ctx.send(embed=embed)
+#     await ctx.send(embed=embed)
 
-# information about bicepcurls
-@client.command() 
-async def bicepcurls(ctx):
-    embed = discord.Embed(
-        title= "How to do bicepcurls",
-        color = discord.Color.purple(),
-        description = "See in GIF below (warning don't use cat)"
-    )
-    embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825276894098227240/9ViG.gif')  
-    await ctx.send(embed=embed)
+# # information about bicepcurls
+# @client.command() 
+# async def bicepcurls(ctx):
+#     embed = discord.Embed(
+#         title= "How to do bicepcurls",
+#         color = discord.Color.purple(),
+#         description = "See in GIF below (warning don't use cat)"
+#     )
+#     embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825276894098227240/9ViG.gif')  
+#     await ctx.send(embed=embed)
 
-# information about squats
-@client.command() 
-async def squats(ctx):
-    embed = discord.Embed(
-        title= "How to do Squats",
-        color = discord.Color.purple(),
-        description = "See in GIF below (warning dog compulsary)"
-    )
-    embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825276133524242472/1q0G.gif')    
-    await ctx.send(embed=embed)
+# # information about squats
+# @client.command() 
+# async def squats(ctx):
+    # embed = discord.Embed(
+    #     title= "How to do Squats",
+    #     color = discord.Color.purple(),
+    #     description = "See in GIF below (warning dog compulsary)"
+    # )
+    # embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825276133524242472/1q0G.gif')    
+    # await ctx.send(embed=embed)
 
 
 # Test command, tells the latency of the bot
