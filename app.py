5e75e794a18d2af93105503a02782c3e5c3d3d4d
Eclipsu <stharj219@gmail.com>
2021-03-31 21:56:30 +0545

    Fixed bug: when <3 schedule was used on friday

--- app.py
+++ app.py
@@ -18,15 +18,13 @@ def get_token():
 
 client = commands.Bot(command_prefix = "<3 ")   # global bot delcaration.
 
-def get_routine():
+def get_routine(week_day):
     """
     get details about routines from routine.json
-
+    Takes:
+        week_day = week of the day eg: sunday, monday tuesday.
     Args:
         openfile, json_object, pairs = variables req by json module (honestly idk what it does). 
-        cr_year, cr_month, cr_day    = Current year/month/day.
-        week_days                    = array of weeks (starts with monday and ends with monday to fit internation week format).
-        week_num                     = a int which acts as index to get current week from week_days array.
         today_routine                = return value containing informations about the workouts.
     Returns:
         wokrout, video, thumnail_link, gif_link, rounds, etc.
@@ -35,37 +33,31 @@ def get_routine():
     with open('routine.json', 'r') as openfile:
         json_object = json.load(openfile)
         pairs = json_object.items()
+        
+        routine_title = json_object[str(week_day)]['title']
+        _1_title = json_object[str(week_day)]['1']['name']
+        _1_reps  = json_object[str(week_day)]['1']['reps']
+        _1_link  = json_object[str(week_day)]['1']['link']
 
-        week_days = ['monday', 'tuesday', 'wednesday', 'thirsday', 'friday', 'saturday', 'sunday']
-        cr_year = datetime.today().strftime('%Y')
-        cr_month = datetime.today().strftime('%m')
-        cr_day = datetime.today().strftime('%d')
-        week_num  = dt.date(int(cr_year), int(cr_month), int(cr_day)).weekday() 
+        _2_title = json_object[str(week_day)]['2']['name']
+        _2_reps  = json_object[str(week_day)]['2']['reps']
+        _2_link  = json_object[str(week_day)]['2']['link']
 
-        routine_title = json_object[str(week_days[week_num])]['title']
-        _1_title = json_object[str(week_days[week_num])]['1']['name']
-        _1_reps  = json_object[str(week_days[week_num])]['1']['reps']
-        _1_link  = json_object[str(week_days[week_num])]['1']['link']
+        _3_title = json_object[str(week_day)]['3']['name']
+        _3_reps  = json_object[str(week_day)]['3']['reps']
+        _3_link  = json_object[str(week_day)]['3']['link']
 
-        _2_title = json_object[str(week_days[week_num])]['2']['name']
-        _2_reps  = json_object[str(week_days[week_num])]['2']['reps']
-        _2_link  = json_object[str(week_days[week_num])]['2']['link']
+        _4_title = json_object[str(week_day)]['4']['name']
+        _4_reps  = json_object[str(week_day)]['4']['reps']
+        _4_link  = json_object[str(week_day)]['4']['link']
 
-        _3_title = json_object[str(week_days[week_num])]['3']['name']
-        _3_reps  = json_object[str(week_days[week_num])]['3']['reps']
-        _3_link  = json_object[str(week_days[week_num])]['3']['link']
+        _5_title = json_object[str(week_day)]['5']['name']
+        _5_reps  = json_object[str(week_day)]['5']['reps']
+        _5_link  = json_object[str(week_day)]['5']['link']
 
-        _4_title = json_object[str(week_days[week_num])]['4']['name']
-        _4_reps  = json_object[str(week_days[week_num])]['4']['reps']
-        _4_link  = json_object[str(week_days[week_num])]['4']['link']
-
-        _5_title = json_object[str(week_days[week_num])]['5']['name']
-        _5_reps  = json_object[str(week_days[week_num])]['5']['reps']
-        _5_link  = json_object[str(week_days[week_num])]['5']['link']
-
-        _6_title = json_object[str(week_days[week_num])]['6']['name']
-        _6_reps  = json_object[str(week_days[week_num])]['6']['reps']
-        _6_link  = json_object[str(week_days[week_num])]['6']['link']
+        _6_title = json_object[str(week_day)]['6']['name']
+        _6_reps  = json_object[str(week_day)]['6']['reps']
+        _6_link  = json_object[str(week_day)]['6']['link']
 
     return routine_title, _1_title, _1_reps, _1_link, _2_title, _2_reps, _2_link, _3_title, _3_reps, _3_link, _4_title, _4_reps, _4_link, _5_title, _5_reps, _5_link, _6_title, _6_reps, _6_link
 
@@ -80,7 +72,35 @@ async def on_ready():
 
 @client.command()
 async def schedule(ctx):
-    title,_1_title, _1_reps, _1_link,  _2_title, _2_reps, _2_link, _3_title, _3_reps, _3_link, _4_title, _4_reps, _4_link, _5_title, _5_reps, _5_link, _6_title, _6_reps, _6_link = get_routine()
+    """
+    displays the schedule of the day (today).
+    Args:
+        cr_year, cr_month, cr_day    = Current year/month/day.
+        week_days                    = array of weeks (starts with monday and ends with monday to fit internation week format).
+        week_num                     = a int which acts as index to get current week from week_days array.
+        title......**                = retuned value which contains information about the embed.
+
+        schedule_embed               = embed containing info about todays schedule.
+    """
+
+    week_days = ['monday', 'tuesday', 'wednesday', 'thirsday', 'friday', 'saturday', 'sunday']
+    cr_year   = datetime.today().strftime('%Y')
+    cr_month  = datetime.today().strftime('%m')
+    cr_day    = datetime.today().strftime('%d')
+    week_num  = 4 
+
+    if week_num == 4: #if its friday (rest day)
+        rest_embed = discord.Embed(
+            title = f'🛌: Rest! ' ,
+            color = discord.Color.purple(),
+            description = 'Great! You did awsome! Lets get some rest, and call this a skip day today!'
+        )
+        rest_embed.set_image(url = 'https://github.com/Eclipsu/Fitness-Bot/blob/MAIN/assets/rest.gif')
+        rest_embed.set_thumbnail(url = 'https://github.com/Eclipsu/Fitness-Bot/blob/MAIN/assets/rest.webp')
+        await ctx.send(embed = rest_embed)
+        return
+
+    title,_1_title, _1_reps, _1_link,  _2_title, _2_reps, _2_link, _3_title, _3_reps, _3_link, _4_title, _4_reps, _4_link, _5_title, _5_reps, _5_link, _6_title, _6_reps, _6_link = get_routine(week_days[week_num])
     schedule_embed = discord.Embed(
         title = f'📋 {title}',
         color = discord.Color.purple(),
@@ -92,12 +112,19 @@ async def schedule(ctx):
     schedule_embed.add_field(name = _4_title,  value  = f"[{_4_reps}]({_4_link}) reps", inline = True)
     schedule_embed.add_field(name = _5_title,  value  = f"[{_5_reps}]({_5_link}) reps", inline = True)
     schedule_embed.add_field(name = _6_title,  value  = f"[{_6_reps}]({_6_link}) reps", inline = True)
-    schedule_embed.add_field(text = 'DM us for more info')
+    schedule_embed.set_footer(text = 'DM us for more info')
+
     await ctx.send(embed = schedule_embed)
 
 # lists outs schedule of the week
 @client.command()
 async def routine(ctx):
+    """
+    Displays routine of the whole week.
+    Args:
+        routine_embed = Embed with information about the routine of the whole week.
+    """"
+
     routine_embed = discord.Embed(
         title = "\📋: Routine",
         color = discord.Color.purple(),
@@ -111,10 +138,8 @@ async def routine(ctx):
     routine_embed.add_field(name = "Sat",  value  = "Pull day")
     routine_embed.set_thumbnail(url = 'https://media4.giphy.com/media/3o7qE4gcYTW1zZPkre/source.gif')
     routine_embed.set_footer(text = 'Friday rest  🛌')
+
     await ctx.send(embed = routine_embed)
-
-
-
 
 # @client.command() 
 # async def schedule(ctx):
@@ -210,19 +235,22 @@ async def routine(ctx):
     # embed.set_image(url='https://media.discordapp.net/attachments/813017363443482645/825276133524242472/1q0G.gif')    
     # await ctx.send(embed=embed)
 
-
-# Test command, tells the latency of the bot
 @client.command()
 async def ping(ctx):
+    """
+    Displays the latency of the bot (TEST COMMAND)
+    """
+    
     await ctx.send(f'Pong! 🏓 {round(client.latency*1000)}ms')
 
-
 async def check_reminder():
-    """a function loop for checking reminder every sec.
+    """
+    a function loop for checking reminder every sec.
     Args:
         now: current time
           x: workout times
     """
+    
     while(True):
         await asyncio.sleep(1)
         now = datetime.now()
@@ -232,7 +260,6 @@ async def check_reminder():
             channel = client.get_channel(813017363443482645)
             await channel.send(f'<@&{825697640560853004}> its time to workout!!')
         
-
 client.loop.create_task(check_reminder())
 client.run(get_token())
 
